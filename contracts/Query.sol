5a467deca1cd86e85ff9941e4128b3c3fad1a101
chainvisions <chainvisions@outlook.com>
2021-04-12 16:46:42 -0600

    feat: Query with signature

--- contracts/Query.sol
+++ contracts/Query.sol
@@ -12,13 +12,25 @@ contract Query {
     * Allows for querying the balances of all tokens specified.
     */
     function query(address _target, address[] memory _tokens) public view returns (uint256[] memory) {
-        uint256 targetLen = _tokens.length - 1;
-        uint256[] memory balances = new uint256[](targetLen);
+        uint256[] memory balances = new uint256[](_tokens.length);
         for(uint256 i = 0; i<_tokens.length; i++) {
             uint256 bal = IERC20(_tokens[i]).balanceOf(_target);
             balances[i] = bal;
         }
         return balances;
+    }
+
+    /*
+    * Allows for a contract using a specific function signature.
+    */
+
+    function queryWithSignature(address _target, address[] memory _addresses, string memory _signature) public view returns (uint256[] memory) {
+        uint256[] memory numbers = new uint256[](_addresses.length);
+        for(uint256 i = 0; i < _addresses.length; i++) {
+            (, bytes memory data) = _addresses[i].staticcall(abi.encodeWithSignature(_signature, _target));
+            numbers[i] = abi.decode(data, (uint256));
+        }
+        return numbers;
     }
 
 }
